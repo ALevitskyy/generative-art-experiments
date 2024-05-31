f995ed42ef6789df6df64ce06a2caa5fe872d251
Andriy Levitskyy <levitskyyandriy@gmail.com>
2024-05-30 21:02:11 -0400

    First attempts at generative design

---
+++ src/generative-design/P-1-0-01.clj
@@ -0,0 +1,27 @@
+(ns generative-design.P-1-0-01
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(defn setup []
+  (q/frame-rate 30)
+  (q/no-cursor)
+  (q/color-mode :hsb 360 100 100)
+  (q/rect-mode :center)
+  (q/no-stroke))
+
+(defn draw-state [state]
+  (q/background (/ (q/mouse-y) 2) 100 100)
+  (q/fill (- 360 (/ (q/mouse-x) 2)) 100 100)
+  (q/rect 360 360 (inc (q/mouse-x)) (inc (q/mouse-y))))
+
+(defn key-pressed [state {:keys [key key-code]}]
+  (when (or (= key :s) (= key :S))
+    (q/save "/Users/andriylevitskyy/Desktop/generative_art/P_1_0_01.jpg")))
+
+(q/defsketch sketch
+  :title "P_1_0_01"
+  :size [720 720]
+  :setup setup
+  :draw draw-state
+  :middleware [m/fun-mode]
+  :key-pressed key-pressed)
---
+++ src/generative-design/P-1-1-1-01.clj
@@ -0,0 +1,32 @@
+(ns generative-design.P-1-1-1-01
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(def width 800)
+(def height 400)
+(defn setup []
+  (q/frame-rate 30)
+  (q/color-mode :hsb width height 100)
+  (q/no-stroke))
+
+(defn draw-state [state]
+  (let [stepX (+ (q/mouse-x) 2)
+        stepY (+ (q/mouse-y) 2)
+        gridX (range 0 width stepX)
+        gridY (range 0 height stepY)
+        gridXY (for [x gridX y gridY] [x y])]
+    (doseq [[x y] gridXY]
+      (q/fill x (- height y) 100)
+      (q/rect x y stepX stepY))))
+
+(defn key-pressed [state {:keys [key key-code]}]
+  (when (or (= key :s) (= key :S))
+    (q/save "/Users/andriylevitskyy/Desktop/generative_art/P_1_1_1_01.jpg")))
+
+(q/defsketch sketch
+  :title "P-1-1-1-01"
+  :size [width height]
+  :setup setup
+  :draw draw-state
+  :middleware [m/fun-mode]
+  :key-pressed key-pressed)
