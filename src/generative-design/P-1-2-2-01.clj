291b7cf874284d00855430ee4467d1de6c6bb48b
Andriy Levitskyy <levitskyyandriy@gmail.com>
2024-06-01 15:19:29 -0400

    Fix the problems with exercise 5 of generative design

--- src/generative-design/P-1-2-2-01.clj
+++ src/generative-design/P-1-2-2-01.clj
@@ -1,62 +1,36 @@
 (ns generative-design.P-1-2-2-01
-  (:require [quil.core :as q]
-            [quil.middleware :as m]))
+  [:require [quil.core :as q]
+   [quil.middleware :as m]])
+
 
 (def image1 "/Users/andriylevitskyy/Desktop/generative_art/generative-art-experiments/src/generative-design/data/P-1-2-2-01/data/pic1.jpg")
-(def image2 "/Users/andriylevitskyy/Desktop/generative_art/generative-art-experiments/src/generative-design/data/P-1-2-2-01/data/pic2.jpg")
-(def image3 "/Users/andriylevitskyy/Desktop/generative_art/generative-art-experiments/src/generative-design/data/P-1-2-2-01/data/pic3.jpg")
-(def image4 "/Users/andriylevitskyy/Desktop/generative_art/generative-art-experiments/src/generative-design/data/P-1-2-2-01/data/pic4.jpg")
 (def image (atom nil))
-(def sortMode (atom nil))
 
 (defn setup []
-  (q/frame-rate 1)
-  (q/no-cursor)
+  (q/frame-rate 15)
   (q/no-stroke)
+  (q/color-mode :rgb 255)
   (reset! image (q/load-image image1)))
-
-(defn max-seq [s]
-  (apply max s))
 
 (defn draw-state [state]
   (let [tile-count (q/floor (/ (q/width) (max 5 (q/mouse-x))))
         rect-size (/ (q/width) tile-count)
+        _ (println "rect-size" rect-size "tile-count" tile-count)
         pixels (q/pixels @image)
         grid-x (range 0 tile-count)
         grid-y (range 0 tile-count)
         grid-xy (for [x grid-x y grid-y] [x y])
-        px (map #(int (* (first %) rect-size)) grid-xy)
-        py (map #(int (* (second %) rect-size)) grid-xy)
-        image-width (. @image width)
-        i (for [x px y py] (* 4 (+ x (* y image-width))))
-        ;_ (println (count pixels) (max-seq i))
-        ; Need to figure out why index overflows
-        colors (map #(q/color (nth pixels %) (nth pixels (inc %)) (nth pixels (+ 2 %)) (nth pixels (+ 3 %))) i)
-        sorted-colors colors
-        params (map vector px py sorted-colors)]
-    (doseq [[x y col] params]
-      (q/fill col)
-      (q/rect (* x rect-size) (* y rect-size) rect-size rect-size))))
-
-
-(defn key-pressed [state {:keys [key key-code]}]
-  (case key
-    :s (q/save "/Users/andriylevitskyy/Desktop/generative_art/P_1_2_2_01.jpg")
-    :S (q/save "/Users/andriylevitskyy/Desktop/generative_art/P_1_2_2_01.jpg")
-    :1 (reset! image (q/load-image image1))
-    :2 (reset! image (q/load-image image2))
-    :3 (reset! image (q/load-image image3))
-    :4 (reset! image (q/load-image image4))
-    :5 (reset! sortMode nil)
-    :6 (reset! sortMode :hue)
-    :7 (reset! sortMode :saturation)
-    :8 (reset! sortMode :brightness)
-    :9 (reset! sortMode :grayscale)))
+        grid-xx (map first grid-xy)
+        grid-yy (map second grid-xy)
+        is (for [x grid-x y grid-y] (+ (* (* y rect-size) (q/width)) (* x rect-size)))
+        colors (map #(nth pixels %) is)
+        params (map vector grid-xx grid-yy colors)]
+    (doseq [[x y col] params] (q/fill (q/red col) (q/green col) (q/blue col))
+           (q/rect (* x rect-size) (* y rect-size) rect-size rect-size))))
 
 (q/defsketch sketch
-  :title "P-1-1-1-01"
+  :title "test"
   :size [600 600]
   :setup setup
   :draw draw-state
-  :middleware [m/fun-mode]
-  :key-pressed key-pressed)
+  :middleware [m/fun-mode])
