c007392e190e1d246c1f33eeced2ce3f5622cdd4
Andriy Levitskyy <levitskyyandriy@gmail.com>
2024-06-27 12:25:33 -0400

    Remove DS Store, strugglung with an exercise

--- src/generative-design/P-1-2-3-03.clj
+++ src/generative-design/P-1-2-3-03.clj
@@ -17,6 +17,14 @@
   (q/color-mode :hsb 360 100 100 100)
   (q/no-stroke))
 
+(defn ensure-vector [input]
+  (println input)
+  (cond
+    (float? input) [input] ; If input is a float, wrap it in a vector
+    (vector? input) input
+    (list? input) input; If input is already a vector, return it as is
+    :else (throw (IllegalArgumentException. "Input must be either a float or a vector"))))
+
 (defn get-parts [i]
   (let [partCount (+ i 1)
         mask (repeatedly partCount #(< (q/random 1) 0.075))
@@ -24,17 +32,31 @@
         generator #(q/random 2)
         parts (map #(repeatedly % generator) fragments)
         else-branch (repeatedly partCount #(q/random 2))
-        params (map vector mask parts else-branch)]
-    (mapcat (fn [[mask parts else-branch]]
-              (if mask parts else-branch)) params)))
+        params (map vector mask parts else-branch)
+        _ (println parts)
+        __ (println else-branch)]
+    (mapcat (fn [[mask-i parts-i else-branch-i]]
+              (if mask-i (ensure-vector parts-i) (ensure-vector else-branch-i))) params)))
 
-(defn gradient [x y w h c1 c2]
-  (let [ctx (q/get-sketch-by-id "sketch")
-        grd (.createLinearGradient ctx x y (+ x w) y)]
-    (.addColorStop grd 0 (.toString c1))
-    (.addColorStop grd 1 (.toString c2))
-    (.setFillStyle ctx grd)
-    (.fillRect ctx x y w h)))
+
+
+;(defn gradient [x y w h c1 c2]
+;  (let [ctx (q/get-sketch-by-id "sketch")
+;        grd (.createLinearGradient ctx x y (+ x w) y)]
+;    (.addColorStop grd 0 (.toString c1))
+;    (.addColorStop grd 1 (.toString c2))
+;    (.setFillStyle ctx grd)
+;    (.fillRect ctx x y w h)))
+
+(defn gradient-v2 [x y w h c1 c2]
+  (q/begin-shape)
+  (q/fill c1)
+  (q/vertex x y)
+  (q/vertex (+ x w) y)
+  (q/fill c2)
+  (q/vertex (+ x w) (+ y h))
+  (q/vertex x (+ y h))
+  (q/end-shape))
 
 (defn draw-state [state]
   (let
@@ -47,7 +69,9 @@
     rowCount (int (q/random 5 30))
     rowHeight (/ (q/height) rowCount)
     index (range rowCount -1 -1)
+    _ (println index)
     parts_ (map get-parts index)
+    __ (println parts_)
     counts (reductions + (map #(count %) parts_))
     params (map vector index parts_ counts)
     draw-row (fn  [i parts end-count]
@@ -63,7 +87,7 @@
                      col1  (repeatedly N   #(q/color 0))
                      col2 (map #(q/color (nth hueValues %) (nth saturationValues %) (nth brightnessValues %) alphaValue) index)
                      row-params (map vector x y w h col1 col2)]
-                 (doseq [[x y w h col1 col2] row-params] (gradient x y w h col1 col2))))]
+                 (doseq [[x y w h col1 col2] row-params] (gradient-v2 x y w h col1 col2))))]
     (doseq [[i parts end-count] params] (draw-row i parts end-count))))
 
 
@@ -84,3 +108,5 @@
   :middleware [m/fun-mode]
   :key-pressed key-pressed
   :mouse-released mouse-released)
+
+
